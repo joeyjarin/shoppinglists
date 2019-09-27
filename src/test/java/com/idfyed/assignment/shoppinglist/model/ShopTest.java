fec0fe7b67e6949a75ddd96469d26ec13298e908
joeyjarin <joeyjarin@yahoo.com>
2019-09-27 21:54:21 +0800

    fixed ShopTest and Shop model

--- src/test/java/com/idfyed/assignment/shoppinglist/model/ShopTest.java
+++ src/test/java/com/idfyed/assignment/shoppinglist/model/ShopTest.java
@@ -10,6 +10,14 @@ public class ShopTest {
 		Shop target = new Shop("acme", 43);
 
 		Assert.assertEquals(target.getName(), "acme");
+		Assert.assertEquals(target.getId(), 43);
+	}
+	
+	@Test
+	public void getDefaultIdTest() {
+		Shop target = new Shop("acme");
+
+		Assert.assertEquals(target.getName(), "acme");
 		Assert.assertEquals(target.getId(), 42);
 	}
 }
